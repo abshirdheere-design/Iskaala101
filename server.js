5a78a8ee3688efd659958201290e27214a3f68d4
Abshir Hassan <abshirdheere@gmail.com.com>
2026-04-26 17:42:25 +0200

    sixid yer server

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 
 // GLOBAL STATE
 let rooms = {}; 
