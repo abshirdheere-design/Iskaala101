0e33c46f774efab1ab6a47d4a78da0b79edc2e30
Abshir Hassan <abshirdheere@gmail.com.com>
2026-04-26 17:53:44 +0200

    sixid yer server

--- server.js
+++ server.js
@@ -14,10 +14,13 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use(express.static(__dirname));
+
 app.use(
   "/socket.io",
   express.static(path.join(__dirname, "node_modules/socket.io/client-dist"))
 );
+
 
 
 // GLOBAL STATE
